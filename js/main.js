e0a84abed2256e540bce922d1f52f9a71d8403e7
Mbaxtiyorov <mbaxtiyorov366@gmai.com>
2022-04-09 09:25:21 +0500

    connect to git hub

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert ("salom xush kelibsiz"
+
+);
+var question = prompt("what is your name");
+console.log(question);
+var whatdoyouwork= prompt("what do you do");
+console.log(whatdoyouwork);
+var age= prompt( "enter your age")
+console.log(age);
+
+console.log("ismingiz"+question+whatdoyouwork+age)
